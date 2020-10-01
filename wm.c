857c4844196f1c18fcf125479a544db939326846
Jeffrey E. Bedard <jefbed@gmail.com>
2020-10-01 01:22:36 -0400

    Implemented sloppy focus.
    Processed windows existing before program start, configuring to listen
    for enter events.

--- wm.c
+++ wm.c
@@ -9,7 +9,6 @@
 #define WM_TERMINAL_COMMAND "xterm"
 enum KeyCodeEnum {
   Tab=23, Left=43, Down=44, Up=45, Right=46, Enter=36
-
 };
 int main(int const argc __attribute__((unused)),
   char const ** argv __attribute__((unused))) {
@@ -27,6 +26,7 @@ int main(int const argc __attribute__((unused)),
   xcb_key_press_event_t * KeyPress;
   xcb_button_press_event_t * ButtonPress;
   xcb_motion_notify_event_t * Motion;
+  xcb_query_tree_cookie_t QueryCookie;
   xcb_setup_t const * Setup;
   xcb_screen_iterator_t ScreenIterator;
   xcb_screen_t * Screen;
@@ -48,19 +48,41 @@ int main(int const argc __attribute__((unused)),
   ScreenIterator = xcb_setup_roots_iterator(Setup);
   Screen = ScreenIterator.data;
   Root = Screen->root;
+  QueryCookie = xcb_query_tree(X, Root);
 
-  /* Grab events.  */
+  /* Determine if another window manager is running.  */
   Values[0] = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT;
-  Cookie = xcb_change_window_attributes_checked(X, Root,
-    XCB_CW_EVENT_MASK, Values);
+  Cookie=xcb_change_window_attributes_checked(X, Root, XCB_CW_EVENT_MASK,
+    Values);
   if (xcb_request_check(X, Cookie)) {
     /* Another window manager is running. */
     fputs("OCCUPIED\n", stderr);
     xcb_disconnect(X);
     exit(1);
   }
+#ifndef WM_HANDLE_CONFIGURE_REQUEST
+  /* Reset grabbed events.  */
   Values[0] = 0;
   xcb_change_window_attributes(X, Root, XCB_CW_EVENT_MASK, Values);
+#endif // ! WM_HANDLE_CONFIGURE_REQUEST
+
+  /* Get existing windows, listen for enter event for sloppy focus.  */
+  {
+    xcb_query_tree_reply_t * Query;
+    xcb_window_t * Children;
+    int Length;
+    Query = xcb_query_tree_reply(X, QueryCookie, NULL);
+    Children = xcb_query_tree_children(Query);
+    Length = xcb_query_tree_children_length(Query);
+    while (Length--) {
+      Values[0] = XCB_EVENT_MASK_ENTER_WINDOW;
+      xcb_change_window_attributes(X, Children[Length], XCB_CW_EVENT_MASK,
+        Values);
+      fprintf(stderr, "%d ", Children[Length]);
+    }
+    fprintf(stderr, "\n");
+    free(Query);
+  }
 
   /* Grab buttons. */
   xcb_grab_button(X, 0, Root, XCB_EVENT_MASK_BUTTON_PRESS |
@@ -84,20 +106,23 @@ int main(int const argc __attribute__((unused)),
     switch (Event->response_type & ~0x80) {
     case XCB_BUTTON_PRESS:
       ButtonPress = (xcb_button_press_event_t*)Event;
-      /* #define DEBUG_XCB_BUTTON_PRESS */
-#ifdef DEBUG_XCB_BUTTON_PRESS
+      /* #define WM_DEBUG_XCB_BUTTON_PRESS */
+#ifdef WM_DEBUG_XCB_BUTTON_PRESS
       fprintf(stderr, "%x %x %x %x %x\n", ButtonPress->event,
         ButtonPress->child, ButtonPress->root, ButtonPress->state,
         ButtonPress->detail);
-#endif /* DEBUG_XCB_BUTTON_PRESS */
+#endif /* WM_DEBUG_XCB_BUTTON_PRESS */
       Window = ButtonPress->child;
+      xcb_set_input_focus(X, XCB_INPUT_FOCUS_POINTER_ROOT, Window,
+        XCB_CURRENT_TIME);
       if (Window) {
         GeometryCookie = xcb_get_geometry(X, Window);
         switch (ButtonPress->detail) {
         case 1: /* move */
           IsResizing = false;
           Values[0] = XCB_STACK_MODE_ABOVE;
-          xcb_configure_window(X, Window, XCB_CONFIG_WINDOW_STACK_MODE, Values);
+          xcb_configure_window(X, Window, XCB_CONFIG_WINDOW_STACK_MODE,
+            Values);
           xcb_grab_pointer(X, 0, Root, XCB_EVENT_MASK_BUTTON_MOTION |
             XCB_EVENT_MASK_BUTTON_RELEASE,
             XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, Root, XCB_NONE,
@@ -138,25 +163,53 @@ int main(int const argc __attribute__((unused)),
       xcb_ungrab_pointer(X, XCB_CURRENT_TIME);
       xcb_flush(X);
       break;
+    case XCB_ENTER_NOTIFY: {
+      xcb_enter_notify_event_t * Enter;
+      Enter = (xcb_enter_notify_event_t *)Event;
+      Window = Enter->event;
+      xcb_set_input_focus(X, XCB_INPUT_FOCUS_POINTER_ROOT, Window,
+        XCB_CURRENT_TIME);
+      xcb_flush(X);
+      break;
+    }
     case XCB_KEY_PRESS:
       KeyPress = (xcb_key_press_event_t*)Event;
-#define DEBUG_XCB_KEY_PRESS
-#ifdef DEBUG_XCB_KEY_PRESS
+#define WM_DEBUG_XCB_KEY_PRESS
+#ifdef WM_DEBUG_XCB_KEY_PRESS
       fprintf(stderr, "KEY %d\n", (int)KeyPress->detail);
-#endif /* DEBUG_XCB_KEY_PRESS */
+#endif /* WM_DEBUG_XCB_KEY_PRESS */
       switch(KeyPress->detail) {
       case Enter:
         system(WM_TERMINAL_COMMAND "&");
         break;
       }
       break;
+#ifdef WM_HANDLE_CONFIGURE_REQUEST
+    case XCB_CONFIGURE_REQUEST: {
+      xcb_configure_request_event_t * Configure;
+      Configure = (xcb_configure_request_event_t *)Event;
+      Window = Configure->window;
+      fprintf(stderr, "XCB_CONFIGURE_REQUEST\n");
+      Values[0] = Configure->x;
+      Values[1] = Configure->y;
+      Values[2] = Configure->width;
+      Values[3] = Configure->height;
+      Mask= XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT |
+        XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y;
+      xcb_configure_window(X, Window, Mask, Values);
+      /* Grab events.  */
+      xcb_map_window(X, Window);
+      xcb_flush(X);
+      break;
+    }
+#endif // WM_HANDLE_CONFIGURE_REQUEST
     case XCB_MOTION_NOTIFY:
       Motion = (xcb_motion_notify_event_t *)Event;
-//#define DEBUG_XCB_MOTION_NOTIFY
-#ifdef DEBUG_XCB_MOTION_NOTIFY
+      //#define WM_DEBUG_XCB_MOTION_NOTIFY
+#ifdef WM_DEBUG_XCB_MOTION_NOTIFY
       fprintf(stderr, "Window: %d, IsResizing:%d, Values[0]:%d, Values[1]:%d\n",
         Window, IsResizing, Values[0], Values[1]);
-#endif /* DEBUG_XCB_MOTION_NOTIFY */
+#endif /* WM_DEBUG_XCB_MOTION_NOTIFY */
       Values[0] = Motion->event_x-Start[0];
       Values[1] = Motion->event_y-Start[1];
       Mask=IsResizing ? XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT
