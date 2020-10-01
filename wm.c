29becb619d1326219c973f1b0110a0c88c4d102b
Jeffrey E. Bedard <jefbed@gmail.com>
2020-09-30 22:53:42 -0400

    Implemented window resize.
    Added debug sections which may be enabled with macros.  Disabled key
    processing until it is implemented.

--- wm.c
+++ wm.c
@@ -1,5 +1,5 @@
 /* wm - X11 Window Manager */
-/* Based on tinywm-xcb.  */
+/* Based on tinywm-xcb. */
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -12,10 +12,10 @@ enum KeyCodeEnum {
 };
 int main(int const ArgumentsCount, char const ** Arguments) {
   bool IsResizing;
-  /* Declare variables.  */
+  /* Declare variables. */
   uint32_t Values[5];
   /* This is the offset from the top left corner of the window at which
-   * dragging starts.  */
+   * dragging starts. */
   short Start[2];
   uint16_t Mask;
   xcb_connection_t * X;
@@ -33,8 +33,8 @@ int main(int const ArgumentsCount, char const ** Arguments) {
   xcb_void_cookie_t Cookie;
   xcb_window_t Root;
   xcb_window_t Window;
-  /* Initialize variables.  */
-  /* Open connection.  */
+  /* Initialize variables. */
+  /* Open connection. */
   X = xcb_connect(NULL,NULL);
   if (xcb_connection_has_error(X)) {
     fputs("DISPLAY\n", stderr);
@@ -42,10 +42,10 @@ int main(int const ArgumentsCount, char const ** Arguments) {
     exit(1);
   }
 
-  /* Generate IDs.  */
+  /* Generate IDs. */
   GraphicsContext = xcb_generate_id(X);
 
-  /* Get screen information.  */
+  /* Get screen information. */
   {
     Setup = xcb_get_setup(X);
     ScreenIterator = xcb_setup_roots_iterator(Setup);
@@ -57,19 +57,19 @@ int main(int const ArgumentsCount, char const ** Arguments) {
   xcb_create_gc(X, GraphicsContext, Root,
     XCB_GC_FUNCTION | XCB_GC_LINE_WIDTH, Values);
 
-  /* Grab events.   */
+  /* Grab events.  */
   Values[0] = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |
     XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY;
   Cookie = xcb_change_window_attributes_checked(X, Root,
     XCB_CW_EVENT_MASK, Values);
   if (xcb_request_check(X, Cookie)) {
-    /* Another window manager is running.  */
+    /* Another window manager is running. */
     fputs("OCCUPIED\n", stderr);
     xcb_disconnect(X);
     exit(1);
   }
 
-  /* Grab buttons.  */
+  /* Grab buttons. */
   xcb_grab_button(X, 0, Root, XCB_EVENT_MASK_BUTTON_PRESS |
     XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC,
     XCB_GRAB_MODE_ASYNC, Root, XCB_NONE, 1, XCB_MOD_MASK_1);
@@ -80,21 +80,24 @@ int main(int const ArgumentsCount, char const ** Arguments) {
     XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC,
     XCB_GRAB_MODE_ASYNC, Root, XCB_NONE, 3, XCB_MOD_MASK_1);
 
-  /* Grab keys.  */
+  /* Grab keys. */
   Cookie = xcb_grab_key(X, 1, Root, XCB_MOD_MASK_CONTROL |
     XCB_MOD_MASK_1, XCB_GRAB_ANY,
     XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
   xcb_flush(X);
 
-  /* Process events.  */
+  /* Process events. */
   for (;;) {
     Event = xcb_wait_for_event(X);
     switch (Event->response_type & ~0x80) {
     case XCB_BUTTON_PRESS:
       ButtonPress = (xcb_button_press_event_t*)Event;
+      /* #define DEBUG_XCB_BUTTON_PRESS */
+#ifdef DEBUG_XCB_BUTTON_PRESS
       fprintf(stderr, "%x %x %x %x %x\n", ButtonPress->event,
         ButtonPress->child, ButtonPress->root, ButtonPress->state,
         ButtonPress->detail);
+#endif /* DEBUG_XCB_BUTTON_PRESS */
       Window = ButtonPress->child;
       if (Window) {
         GeometryCookie = xcb_get_geometry(X, Window);
@@ -144,6 +147,11 @@ int main(int const ArgumentsCount, char const ** Arguments) {
       xcb_flush(X);
     case XCB_KEY_PRESS:
       KeyPress = (xcb_key_press_event_t*)Event;
+#define DEBUG_XCB_KEY_PRESS
+#ifdef DEBUG_XCB_KEY_PRESS
+      fprintf(stderr, "KEY %d\n", (int)KeyPress->detail);
+#endif /* DEBUG_XCB_KEY_PRESS */
+#if 0
       switch(KeyPress->detail) {
       case Left:
         break;
@@ -157,13 +165,16 @@ int main(int const ArgumentsCount, char const ** Arguments) {
         break;
       case Tab:
         break;
-      default:
-        fprintf(stderr, "KEY %d\n", (int)KeyPress->detail);
       }
+#endif /* 0 */
       break;
     case XCB_MOTION_NOTIFY:
       Motion = (xcb_motion_notify_event_t *)Event;
-      Window = Motion->child;
+//#define DEBUG_XCB_MOTION_NOTIFY
+#ifdef DEBUG_XCB_MOTION_NOTIFY
+      fprintf(stderr, "Window: %d, IsResizing:%d, Values[0]:%d, Values[1]:%d\n",
+        Window, IsResizing, Values[0], Values[1]);
+#endif /* DEBUG_XCB_MOTION_NOTIFY */
       Values[0] = Motion->event_x-Start[0];
       Values[1] = Motion->event_y-Start[1];
       Mask=IsResizing ? XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT
