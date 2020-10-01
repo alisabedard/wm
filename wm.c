61fe66b07888c5252697a248df471d3032e8e8ef
Jeffrey E. Bedard <jefbed@gmail.com>
2020-10-01 03:27:14 -0400

    Split out several functions.
    Defined grabButton and processExisting.
    Defined drag and stack.  Reindented.

--- wm.c
+++ wm.c
@@ -11,9 +11,73 @@
 /*  #define WM_MOD_MASK XCB_MOD_MASK_1 */
 /* Uncomment the following to use Super (Windows) as the hot key.  */
 #define WM_MOD_MASK XCB_MOD_MASK_4
+
 enum KeyCodeEnum {
   Tab=23, Left=43, Down=44, Up=45, Right=46, Enter=36
 };
+
+static inline void grabButton(xcb_connection_t * X, xcb_window_t const Root,
+  uint8_t const Button) {
+  xcb_grab_button(X, 0, Root, XCB_EVENT_MASK_BUTTON_PRESS |
+    XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC,
+    XCB_GRAB_MODE_ASYNC, Root, XCB_NONE, Button, WM_MOD_MASK);
+}
+
+static void processExisting(xcb_connection_t * X,
+  xcb_query_tree_cookie_t const Cookie) {
+  /* Get existing windows, listen for enter event for sloppy focus.  */
+  {
+    xcb_query_tree_reply_t * Query;
+    xcb_window_t * Children;
+    int Length;
+    uint32_t Value;
+    Query = xcb_query_tree_reply(X, Cookie, NULL);
+    Children = xcb_query_tree_children(Query);
+    Length = xcb_query_tree_children_length(Query);
+    Value = XCB_EVENT_MASK_ENTER_WINDOW;
+    while (Length--) {
+      xcb_change_window_attributes(X, Children[Length], XCB_CW_EVENT_MASK,
+        &Value);
+#ifdef WM_DEBUG_XCB_QUERY_TREE
+      fprintf(stderr, "%d ", Children[Length]);
+#endif /* WM_DEBUG_XCB_QUERY_TREE */
+    }
+#ifdef WM_DEBUG_XCB_QUERY_TREE
+    fprintf(stderr, "\n");
+#endif /* WM_DEBUG_XCB_QUERY_TREE */
+    free(Query);
+  }
+
+}
+
+static inline void stack(xcb_connection_t * X, xcb_window_t const Window,
+  uint32_t const Value) {
+  xcb_configure_window(X, Window, XCB_CONFIG_WINDOW_STACK_MODE, &Value);
+}
+
+static void drag(xcb_connection_t * X, int16_t * Start, xcb_window_t const Root,
+  xcb_window_t const Window, bool const IsResizing) {
+  xcb_get_geometry_cookie_t GeometryCookie;
+  xcb_get_geometry_reply_t *Geometry;
+  GeometryCookie = xcb_get_geometry(X, Window);
+  stack(X, Window, XCB_STACK_MODE_ABOVE);
+  xcb_grab_pointer(X, 0, Root, XCB_EVENT_MASK_BUTTON_MOTION |
+    XCB_EVENT_MASK_BUTTON_RELEASE,
+    XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, Root, XCB_NONE,
+    XCB_CURRENT_TIME);
+  Geometry = xcb_get_geometry_reply(X, GeometryCookie, NULL);
+  if (IsResizing) {
+    xcb_warp_pointer(X, XCB_NONE, Window, 0,0,0,0, Geometry->width-1,
+      Geometry->height-1);
+    Start[0] = Geometry->x;
+    Start[1] = Geometry->y;
+  } else {
+    Start[0]-=Geometry->x;
+    Start[1]-=Geometry->y;
+  }
+  free (Geometry);
+}
+
 int main(int const argc __attribute__((unused)),
   char const ** argv __attribute__((unused))) {
   bool IsResizing;
@@ -25,8 +89,6 @@ int main(int const argc __attribute__((unused)),
   uint16_t Mask;
   xcb_connection_t * X;
   xcb_generic_event_t * Event;
-  xcb_get_geometry_cookie_t GeometryCookie;
-  xcb_get_geometry_reply_t *Geometry;
   xcb_key_press_event_t * KeyPress;
   xcb_button_press_event_t * ButtonPress;
   xcb_motion_notify_event_t * Motion;
@@ -37,8 +99,7 @@ int main(int const argc __attribute__((unused)),
   xcb_void_cookie_t Cookie;
   xcb_window_t Root;
   xcb_window_t Window;
-  /* Initialize variables. */
-  Geometry = NULL;
+
   /* Open connection. */
   X = xcb_connect(NULL,NULL);
   if (xcb_connection_has_error(X)) {
@@ -65,38 +126,12 @@ int main(int const argc __attribute__((unused)),
     exit(1);
   }
 
-  /* Get existing windows, listen for enter event for sloppy focus.  */
-  {
-    xcb_query_tree_reply_t * Query;
-    xcb_window_t * Children;
-    int Length;
-    Query = xcb_query_tree_reply(X, QueryCookie, NULL);
-    Children = xcb_query_tree_children(Query);
-    Length = xcb_query_tree_children_length(Query);
-    while (Length--) {
-      Values[0] = XCB_EVENT_MASK_ENTER_WINDOW;
-      xcb_change_window_attributes(X, Children[Length], XCB_CW_EVENT_MASK,
-        Values);
-#ifdef WM_DEBUG_XCB_QUERY_TREE
-      fprintf(stderr, "%d ", Children[Length]);
-#endif /* WM_DEBUG_XCB_QUERY_TREE */
-    }
-#ifdef WM_DEBUG_XCB_QUERY_TREE
-    fprintf(stderr, "\n");
-#endif /* WM_DEBUG_XCB_QUERY_TREE */
-    free(Query);
-  }
+  processExisting(X, QueryCookie);
 
   /* Grab buttons. */
-  xcb_grab_button(X, 0, Root, XCB_EVENT_MASK_BUTTON_PRESS |
-    XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC,
-    XCB_GRAB_MODE_ASYNC, Root, XCB_NONE, 1, WM_MOD_MASK);
-  xcb_grab_button(X, 0, Root, XCB_EVENT_MASK_BUTTON_PRESS |
-    XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC,
-    XCB_GRAB_MODE_ASYNC, Root, XCB_NONE, 2, WM_MOD_MASK);
-  xcb_grab_button(X, 0, Root, XCB_EVENT_MASK_BUTTON_PRESS |
-    XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC,
-    XCB_GRAB_MODE_ASYNC, Root, XCB_NONE, 3, WM_MOD_MASK);
+  grabButton(X, Root, 1);
+  grabButton(X, Root, 2);
+  grabButton(X, Root, 3);
 
   /* Grab keys. */
   xcb_grab_key(X, 1, Root, WM_MOD_MASK, XCB_GRAB_ANY, XCB_GRAB_MODE_ASYNC,
@@ -119,45 +154,19 @@ int main(int const argc __attribute__((unused)),
       xcb_set_input_focus(X, XCB_INPUT_FOCUS_POINTER_ROOT, Window,
         XCB_CURRENT_TIME);
       if (Window) {
-        GeometryCookie = xcb_get_geometry(X, Window);
         switch (ButtonPress->detail) {
         case 1: /* move */
           IsResizing = false;
-          Values[0] = XCB_STACK_MODE_ABOVE;
-          xcb_configure_window(X, Window, XCB_CONFIG_WINDOW_STACK_MODE,
-            Values);
-          xcb_grab_pointer(X, 0, Root, XCB_EVENT_MASK_BUTTON_MOTION |
-            XCB_EVENT_MASK_BUTTON_RELEASE,
-            XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, Root, XCB_NONE,
-            XCB_CURRENT_TIME);
-          Geometry = xcb_get_geometry_reply(X, GeometryCookie, NULL);
-          Start[0] = ButtonPress->event_x-Geometry->x;
-          Start[1] = ButtonPress->event_y-Geometry->y;
-          free (Geometry);
-          Geometry = NULL;
-          xcb_flush(X);
+          Start[0] = ButtonPress->event_x;
+          Start[1] = ButtonPress->event_y;
+          drag(X, Start, Root, Window, IsResizing);
           break;
         case 2: /* lower */
-          Values[0] = XCB_STACK_MODE_BELOW;
-          xcb_configure_window(X, Window, XCB_CONFIG_WINDOW_STACK_MODE, Values);
-          xcb_flush(X);
+          stack(X, Window, XCB_STACK_MODE_BELOW);
           break;
         case 3: /* resize */
           IsResizing = true;
-          Values[0] = XCB_STACK_MODE_ABOVE;
-          xcb_configure_window(X, Window, XCB_CONFIG_WINDOW_STACK_MODE, Values);
-          xcb_grab_pointer(X, 0, Root, XCB_EVENT_MASK_BUTTON_MOTION |
-            XCB_EVENT_MASK_BUTTON_RELEASE,
-            XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, Root, XCB_NONE,
-            XCB_CURRENT_TIME);
-          Geometry = xcb_get_geometry_reply(X, GeometryCookie, NULL);
-          xcb_warp_pointer(X, XCB_NONE, Window, 0,0,0,0,
-            Geometry->width-1,Geometry->height-1);
-          Start[0] = Geometry->x;
-          Start[1] = Geometry->y;
-          free(Geometry);
-          Geometry = NULL;
-          xcb_flush(X);
+          drag(X, Start, Root, Window, IsResizing);
           break;
         }
       }
@@ -206,7 +215,6 @@ int main(int const argc __attribute__((unused)),
       Values[0] = XCB_EVENT_MASK_ENTER_WINDOW;
       xcb_change_window_attributes(X, Window, XCB_CW_EVENT_MASK, Values);
       xcb_map_window(X, Window);
-      xcb_flush(X);
       break;
     }
     case XCB_MOTION_NOTIFY:
@@ -221,14 +229,10 @@ int main(int const argc __attribute__((unused)),
       Mask=IsResizing ? XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT
         : XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y;
       xcb_configure_window(X, Window, Mask, Values);
-      xcb_flush(X);
       break;
     }
+    xcb_flush(X);
     free(Event);
-    if (Geometry) {
-      free(Geometry);
-      Geometry = NULL;
-    }
   }
   xcb_disconnect(X);
   return 0;
